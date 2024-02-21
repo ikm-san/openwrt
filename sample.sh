eb051279bdb4b2f67dcda254fa999db64c6e7fa6
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-21 15:45:12 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# インターフェース名を指定する場合は、この変数を設定
-INTERFACE="br-lan"
+. /lib/functions/network.sh
+network_flush_cache
+network_find_wan6 NET_IF6
+network_get_ipaddr6 NET_ADDR6 "${NET_IF6}"
+new_ip6_prefix=${NET_ADDR6}
 
-# IPv6アドレスを表示
-if [ -z "$INTERFACE" ]; then
-    # インターフェース名が指定されていない場合、全てのインターフェースのIPv6アドレスを表示
-    ip -6 addr
-else
-    # 特定のインターフェースのIPv6アドレスを表示
-    ip -6 addr show dev $INTERFACE
-fi
-
+echo $NET_IF6
+echo new_ip6_prefix

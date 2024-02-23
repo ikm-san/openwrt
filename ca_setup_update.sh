baff14157bded43c935ceea1c8ffc8f00dea633a
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-23 18:15:20 +0900

    Update ca_setup_update.sh

--- ca_setup_update.sh
+++ ca_setup_update.sh
@@ -4,6 +4,5 @@ wget -O /usr/lib/lua/luci/controller/ca_setup.lua https://raw.githubusercontent.
 wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua
 chmod +x /usr/lib/lua/luci/controller/ca_setup.lua
 chmod +x /usr/lib/lua/luci/model/cbi/ca_setup/*.lua
-
 rm -rf /tmp/luci-*
 /etc/init.d/uhttpd restart

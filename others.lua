d29cc35d4796b6f38b1186ede70a7890a6e4edf4
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-24 10:12:23 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,11 +1,15 @@
-m = Map("others", "その他の設定") -- "others"は設定ファイル名（/etc/config/othersに対応）、"その他の設定"はこのセクションのタイトルです。
+m = Map(nil, "その他の設定") -- 設定ファイルに依存しない
 
 s = m:section(TypedSection, "others_section", "セクションの説明")
-s.addremove = true -- セクションの追加と削除を許可
+s.addremove = false -- セクションの追加と削除を不可に
 s.anonymous = true -- 名前のないセクションを許可
 
 o = s:option(Value, "example_option", "例のオプション", "このオプションの説明。")
 o.datatype = "string" -- 入力値の型を指定
 o.default = "デフォルト値" -- デフォルト値の設定
+
+function m.on_commit(map)
+    -- ここにフォーム送信時の処理を記述
+end
 
 return m

79e8485dafe0a1d5b0a7232d7a4a51a7d346f5ef
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 15:42:15 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,4 +1,3 @@
-local fs = require "nixio.fs"
 local sys = require "luci.sys"
 
 m = Map("network", "WAN接続設定の保存復元")
@@ -11,22 +10,10 @@ op:value("restore", "前回の設定に戻す")
 function m.on_commit(map)
     local selected_op = m:formvalue(op:cbid())
     if selected_op == "save" then
-        if fs.copy("/etc/config/network", "/etc/config/network.old") then
-            -- コピーに成功した場合の処理（省略）
-        else
-            -- コピーに失敗した場合の処理（省略）
-        end
+        sys.call("cp /etc/config/network /etc/config/network.old")
     elseif selected_op == "restore" then
-        if fs.exists("/etc/config/network.old") then
-            if fs.copy("/etc/config/network.old", "/etc/config/network") then
-                sys.call("/etc/init.d/network restart")
-                -- 復元に成功した場合の処理（省略）
-            else
-                -- コピーに失敗した場合の処理（省略）
-            end
-        else
-            -- バックアップファイルが存在しない場合の処理（省略）
-        end
+        sys.call("cp /etc/config/network.old /etc/config/network")
+        sys.call("/etc/init.d/network restart")
     end
 end
 

5e50f9682efb8f9d7a69093dfbb19ef77e9443ab
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 18:46:23 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,22 +1,25 @@
 local sys = require "luci.sys"
 
 -- Mapの定義
-m = Map("network", "WAN接続設定の保存復元", "以下のボタンをクリックして設定を保存または復元")
+m = Map("network", "WAN接続設定の保存復元", "以下の選択肢を選んで設定を保存または復元")
 
 -- SimpleSectionを使用してセクションを定義
 s = m:section(SimpleSection)
 
--- 保存ボタン
-local save = s:option(Button, "_save", "設定を保存")
-function save.write(self)
-    luci.sys.call("cp /etc/config/network /etc/config/network.old")
-end
+-- ラジオボタンのような選択肢
+local choice = s:option(ListValue, "_choice", "操作を選択")
+choice:value("save", "設定を保存")
+choice:value("restore", "設定を復元")
 
--- 復元ボタン
-local restore = s:option(Button, "_restore", "設定を復元")
-function restore.write(self)
-    luci.sys.call("cp /etc/config/network.old /etc/config/network")
-    luci.sys.call("/etc/init.d/network restart")
+-- 実際の処理はMapのcommitトリガーで行う
+function m.on_commit(map)
+    local choice_val = choice:formvalue(s.section)
+    if choice_val == "save" then
+        sys.call("cp /etc/config/network /etc/config/network.old")
+    elseif choice_val == "restore" then
+        sys.call("cp /etc/config/network.old /etc/config/network")
+        sys.call("/etc/init.d/network restart")
+    end
 end
 
 return m

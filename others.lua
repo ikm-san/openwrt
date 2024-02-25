084ced762dcedc1b9680cc50ee7addbfd844761d
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 19:03:16 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,17 +1,12 @@
 local sys = require "luci.sys"
 
--- Mapの定義
 m = Map("network",, "以下の選択肢を選んで設定を保存または復元")
-
--- SimpleSectionを使用してセクションを定義
 s = m:section(SimpleSection)
 
--- ラジオボタンのような選択肢
 local choice = s:option(ListValue, "_choice", "操作を選択")
 choice:value("save", "設定を保存")
 choice:value("restore", "設定を復元")
 
--- 実際の処理はMapのcommitトリガーで行う
 function m.on_commit(map)
     local choice_val = choice:formvalue(s.section)
     if choice_val == "save" then

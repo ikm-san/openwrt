e5d7a510b786f266aae0e48a39b52874ac0385ed
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:54:38 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -2,9 +2,10 @@ local fs = require "nixio.fs"
 local sys = require "luci.sys"
 
 m = Map("network", "WAN接続設定の保存復元")
-s = m:section(SimpleSection, nil,)
+s = m:section(SimpleSection, nil, "設定を以下から選んでください")
 
 local op = s:option(ListValue, "_operation", "操作")
+op:value("", "-") -- デフォルトの選択肢を追加
 op:value("save", "現在の設定を保存")
 op:value("restore", "前回の設定に戻す")
 

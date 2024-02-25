c68ac3c671d8351500be4e85a674ecbf4f079e13
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:09:38 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -1,48 +1,45 @@
--- ファイル: /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 
 m = Map("ca_setup", "IPoE設定")
 
-s = m:section(TypedSection, "ipoe", "接続環境のバックアップ")
-s.anonymous = true
-
-s:option(Button, "_save", "現在の設定を保存").write = function()
-    sys.call("cp /etc/config/network /etc/config/network.config_ipoe.old")
-end
-
-s:option(Button, "_restore", "前回の設定に戻す").write = function()
-    sys.call("cp /etc/config/network.config_ipoe.old /etc/config/network")
-    sys.call("/etc/init.d/network restart")
-end
-
+-- IPv4 over IPv6パッケージのセクション
 s = m:section(TypedSection, "ipoe", "IPv4 over IPv6パッケージ")
 s.anonymous = true
 
-s:option(Button, "_mape", "MAP-E").write = function()
-    sys.call("opkg update && opkg install mape")
+-- MAP-Eのインストールをチェックボックスで選択
+local mape = s:option(Flag, "mape", "MAP-E")
+function mape.write(self, section, value)
+    if value == "1" then
+        sys.call("opkg update && opkg install mape")
+    end
 end
 
-s:option(Button, "_dslite", "DS-LITE").write = function()
-    sys.call("opkg update && opkg install ds-lite")
+-- DS-LITEのインストールをチェックボックスで選択
+local dslite = s:option(Flag, "dslite", "DS-LITE")
+function dslite.write(self, section, value)
+    if value == "1" then
+        sys.call("opkg update && opkg install ds-lite")
+    end
 end
 
+-- 接続設定のセクション
 s = m:section(TypedSection, "ipoe", "接続設定")
 s.anonymous = true
 
--- ここに接続設定のためのボタンを追加します。
--- 例:
-s:option(Button, "_v6plus", "v6プラス").write = function()
-    sys.call("wget -O /tmp/v6plus.sh https://raw.githubusercontent.com/ikm-san/openwrt/main/sample.sh && chmod +x /tmp/v6plus.sh && /tmp/v6plus.sh")
-end
+-- 接続設定をラジオボタンで選択
+local conn = s:option(ListValue, "connection", "接続タイプ")
+conn:value("v6plus", "v6プラス")
+conn:value("v6plusB", "v6プラスB")
 
--- 他の接続設定ボタンも同様に追加
-
-s = m:section(TypedSection, "ipoe", "その他の設定")
-s.anonymous = true
-
-s:option(Button, "_reboot", "ルーターを再起動する").write = function()
-    sys.call("reboot")
+function m.on_commit(map)
+    local connection_type = conn:formvalue(s.section)
+    if connection_type == "v6plus" then
+        sys.call("wget -O /tmp/v6plus.sh https://example.com/v6plus.sh && chmod +x /tmp/v6plus.sh && /tmp/v6plus.sh")
+    elseif connection_type == "v6plusB" then
+        -- v6プラスB用のスクリプトを実行するコマンドをここに記述
+        sys.call("echo 'v6プラスBが選択されました'")
+    end
 end
 
 return m

04ce5b7a1520edd707136caf1b18c29a78a0d471
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 18:57:43 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -15,10 +15,10 @@ choice:value("restore", "設定を復元")
 function m.on_commit(map)
     local choice_val = choice:formvalue(s.section)
     if choice_val == "save" then
-        luci.sys.call("cp /etc/config/network /etc/config/network.old")
+        sys.exec("cp /etc/config/network /etc/config/network.old")
     elseif choice_val == "restore" then
-        luci.sys.call("cp /etc/config/network.old /etc/config/network")
-        luci.sys.call("/etc/init.d/network restart")
+        sys.exec("cp /etc/config/network.old /etc/config/network")
+        sys.exec("/etc/init.d/network restart")
     end
 end
 

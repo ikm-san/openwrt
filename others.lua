20408ad0eb2f5fbcde330b4a7f6360e0647ceb82
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 18:36:18 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -9,14 +9,14 @@ s = m:section(SimpleSection)
 -- 保存ボタン
 local save = s:option(Button, "_save", "設定を保存")
 function save.write(self)
-    sys.call("cp /etc/config/network /etc/config/network.old")
+    luci.sys.call("cp /etc/config/network /etc/config/network.old")
 end
 
 -- 復元ボタン
 local restore = s:option(Button, "_restore", "設定を復元")
 function restore.write(self)
-    sys.call("cp /etc/config/network.old /etc/config/network")
-    sys.call("/etc/init.d/network restart")
+    luci.sys.call("cp /etc/config/network.old /etc/config/network")
+    luci.sys.call("/etc/init.d/network restart")
 end
 
 return m

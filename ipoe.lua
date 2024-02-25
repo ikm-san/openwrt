7b62a1abcfc279e68d8f09a66373e2348bf15ec2
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:59:13 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -1,10 +1,10 @@
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 
-m = Map("network", "WAN接続設定")
+m = Map("network", "WAN接続設定", "WAN接続の設定を保存または復元します。")
 
 -- SimpleSectionを使用して接続設定を表示
-s = m:section(SimpleSection, nil, "設定を以下から選んでください")
+s = m:section(SimpleSection, nil, "設定を以下から選んでください。")
 
 -- 接続設定をラジオボタンで選択
 local conn = s:option(ListValue, "connection_type", "接続タイプ")
@@ -14,11 +14,8 @@ conn:value("v6plus", "v6プラス")
 conn:value("ds-liteA", "ds-liteA")
 conn:value("bridge", "ブリッジモード")
 
--- 選択された接続タイプを取得するためのキーを保存するフィールド
-local key = conn:cbid()
-
-function m.on_after_commit(map)
-    local connection_type = m:formvalue(key)
+function m.on_commit(map)
+    local connection_type = map:formvalue(conn:cbid())
 
     if connection_type == "dhcp" then
         -- DHCP接続に関する設定を更新

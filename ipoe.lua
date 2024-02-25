82f23b1a24728963276a8b4df2fd964c235149e7
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:37:14 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -10,20 +10,26 @@ s.addremove = false
 
 -- 接続設定をラジオボタンで選択
 local conn = s:option(ListValue, "connection_type", "接続タイプ")
-conn:value("none", "選択してください")
+conn:value("dhcp", "DHCP自動")
+conn:value("pppoe", "PPPoE接続")
 conn:value("v6plus", "v6プラス")
 conn:value("ds-liteA", "ds-liteA")
+conn:value("bridge", "ブリッジモード")
 conn.rmempty = false
 
 function m.on_commit(map)
     local connection_type = conn:formvalue(s.section)
 
     if connection_type == "v6plus" then
-        -- v6プラスに関連するコマンドを実行
         sys.call("opkg update && opkg install mape")
     elseif connection_type == "ds-liteA" then
-        -- ds-liteAに関連するコマンドを実行
         sys.call("opkg update && opkg install ds-lite")
+    elseif connection_type == "dhcp" then
+        -- DHCP自動に関連するコマンドを実行
+    elseif connection_type == "bridge" then
+        -- ブリッジモードに関連するコマンドを実行
+    elseif connection_type == "pppoe" then
+        -- PPPoEに関連するコマンドを実行
     end
 end
 

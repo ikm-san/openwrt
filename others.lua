c6ca637cb57e6439dfc6c50307af6dab6a2796b0
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 16:16:13 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,7 +1,12 @@
 local sys = require "luci.sys"
 
-m = Map("ca_setup", "WAN接続設定の保存復元")
-s = m:section(SimpleSection, nil, "設定を以下から選んでください")
+m = Map("network", "WAN接続設定の保存復元") 
+
+-- TypedSection を使用し、例として 'interface' セクションタイプを指定します。
+-- これは network 設定ファイル内の実際のセクションタイプに基づくべきです。
+s = m:section(TypedSection, "interface", "設定を以下から選んでください")
+s.addremove = false -- セクションの追加や削除は不要な場合は false に設定
+s.anonymous = true -- セクションの名前を表示しない
 
 local op = s:option(ListValue, "_operation", "操作")
 op:value("save", "現在の設定を保存")

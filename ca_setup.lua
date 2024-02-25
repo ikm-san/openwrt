6d691b03f4f472d6f694a72528db8c49cc6f706f
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:49:36 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -5,7 +5,7 @@ function index()
     entry({"admin", "ca_setup"}, firstchild(), _("CA SETUP"), 60).dependent = false
     
     -- IPoE設定タブ
-    entry({"admin", "ca_setup", "ipoe"}, cbi("ca_setup/ipoe"), _("IPoE設定"), 10)
+    entry({"admin", "ca_setup", "ipoe"}, cbi("ca_setup/ipoe"), _("WAN接続設定"), 10)
     
     -- その他接続設定タブ
     entry({"admin", "ca_setup", "other"}, cbi("ca_setup/others"), _("設定の復元保存"), 60)

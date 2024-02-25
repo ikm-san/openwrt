f8244931ac5cb328ca6aac2dcb1372f49724c9f6
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 16:20:37 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,25 +1,21 @@
 local sys = require "luci.sys"
 
-m = Map("network", "WAN接続設定の保存復元") 
+m = Map("network", "WAN接続設定の保存復元", "以下のボタンをクリックして設定を保存または復元してください。")
 
--- TypedSection を使用し、例として 'interface' セクションタイプを指定します。
--- これは network 設定ファイル内の実際のセクションタイプに基づくべきです。
-s = m:section(TypedSection, "interface", "設定を以下から選んでください")
-s.addremove = false -- セクションの追加や削除は不要な場合は false に設定
-s.anonymous = true -- セクションの名前を表示しない
+-- ボタン用のセクションを定義
+s = m:section(SimpleSection)
 
-local op = s:option(ListValue, "_operation", "操作")
-op:value("save", "現在の設定を保存")
-op:value("restore", "前回の設定に戻す")
+-- 保存ボタン
+local save = s:option(Button, "_save", "設定を保存")
+function save.write()
+    sys.call("cp /etc/config/network /etc/config/network.old")
+end
 
-function m.on_commit(map)
-    local selected_op = m:formvalue(op:cbid())
-    if selected_op == "save" then
-        sys.call("cp /etc/config/network /etc/config/network.old")
-    elseif selected_op == "restore" then
-        sys.call("cp /etc/config/network.old /etc/config/network")
-        sys.call("/etc/init.d/network restart")
-    end
+-- 復元ボタン
+local restore = s:option(Button, "_restore", "設定を復元")
+function restore.write()
+    sys.call("cp /etc/config/network.old /etc/config/network")
+    sys.call("/etc/init.d/network restart")
 end
 
 return m

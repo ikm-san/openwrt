f4048fdaeb2852b55b0e0be530e7201a0534e7e0
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 10:51:45 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -19,34 +19,3 @@ function restore_btn.write(self, section)
 end
 
 return m
-
-s = m:section(TypedSection, "ipoe", "IPv4 over IPv6パッケージ")
-s.anonymous = true
-
-s:option(Button, "_mape", "MAP-E").write = function()
-    sys.call("opkg update && opkg install mape")
-end
-
-s:option(Button, "_dslite", "DS-LITE").write = function()
-    sys.call("opkg update && opkg install ds-lite")
-end
-
-s = m:section(TypedSection, "ipoe", "接続設定")
-s.anonymous = true
-
--- ここに接続設定のためのボタンを追加します。
--- 例:
-s:option(Button, "_v6plus", "v6プラス").write = function()
-    sys.call("wget -O /tmp/v6plus.sh https://raw.githubusercontent.com/ikm-san/openwrt/main/sample.sh && chmod +x /tmp/v6plus.sh && /tmp/v6plus.sh")
-end
-
--- 他の接続設定ボタンも同様に追加
-
-s = m:section(TypedSection, "ipoe", "その他の設定")
-s.anonymous = true
-
-s:option(Button, "_reboot", "ルーターを再起動する").write = function()
-    sys.call("reboot")
-end
-
-return m

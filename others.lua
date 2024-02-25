ef5cffeb9a69ac33a6ae234ac4cc88699b491562
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-25 11:19:25 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -5,12 +5,11 @@ m = Map("network", "WAN設定バックアップ", "設定の保存と復元")
 s = m:section(SimpleSection, nil, "WAN接続環境のバックアップと復元を以下から選択してください。")
 
 local op = s:option(ListValue, "_operation", "操作")
-op:value("", "-")
 op:value("save", "現在の設定を保存")
 op:value("restore", "前回の設定に戻す")
 
 function m.on_commit(map)
-    local selected_op = op:formvalue(s.section)
+    local selected_op = m:formvalue(op:cbid())
     if selected_op == "save" then
         sys.call("cp /etc/config/network /etc/config/network.config_ipoe.old")
     elseif selected_op == "restore" then

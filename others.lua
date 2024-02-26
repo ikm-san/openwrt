ef8937531f9781a9c3247c448648dc0ecfb77dfa
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-26 11:56:32 +0900

    Update others.lua

--- others.lua
+++ others.lua
@@ -1,25 +1,25 @@
 local sys = require "luci.sys"
 local fs = require "nixio.fs"
 
--- Mapの初期化、ここでは特定の設定ファイルを操作するわけではないため、ダミーのconfig名を使用
-m = Map("dummy", "以下の選択肢を選んで設定を保存または復元",
-        "このフォームは実際の設定ファイルを操作しません。")
+-- Mapの初期化、'ca_setup' configファイルを使用
+m = Map("ca_setup", "ネットワーク設定のバックアップ",
+        ネットワーク設定を保存または復元できます。")
 
--- セクションの追加
-s = m:section(TypedSection, "dummy", "操作を選択")
+-- 'backup'セクションの追加
+s = m:section(TypedSection, "backup", "バックアップオプション")
 s.addremove = false
 s.anonymous = true
 
--- 操作の選択肢を追加
-choice = s:option(ListValue, "choice", "操作")
+-- 'network_config_save'選択肢の追加
+choice = s:option(ListValue, "network_config", "操作")
 choice:value("save", "設定を保存")
 choice:value("restore", "設定を復元")
 
--- このfunctionを`choice`のwrite属性に割り当て、SAVE&APPLY時に実行されるようにします
-function choice.write(self, section, value)
-    if value == "save" then
+function m.on_commit(map)
+    local choice_val = m.uci:get("ca_setup", "backup", "network_config")
+    if choice_val == "save" then
         luci.sys.exec("cp /etc/config/network /etc/config/network.old")
-    elseif value == "restore" then
+    elseif choice_val == "restore" then
         luci.sys.exec("cp /etc/config/network.old /etc/config/network")
         luci.sys.exec("/etc/init.d/network restart")
     end

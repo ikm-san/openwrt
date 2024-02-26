4732643f9f47534e41688356504832c8e2323ab9
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-26 15:51:10 +0900

    Update ca_setup_update.sh

--- ca_setup_update.sh
+++ ca_setup_update.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+#network configファイルのバックアップ
+cp /etc/config/network /etc/config/network.old
 
+#不足している場合に備え、必要なソフトウェアのインストール
 opkg update
 opkg install lua luci-base
 opkg install luci-lua-runtime
 opkg install liblucihttp-lua
 opkg install luci-app-uhttpd
 
+#CAセットアップメニュー用のファイル、キャッシュのクリア、Luciサービス再起動
 wget -O /usr/lib/lua/luci/controller/ca_setup.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua
 wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua
 wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup

ffba4acec725677ca58498bbb4a634a97af8cc92
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-02-28 13:39:23 +0900

    Update mape_32.sh

--- mape_32.sh
+++ mape_32.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 
-# 必要なライブラリの読み込み
-. /lib/functions/network.sh
-
-# 定義されたルール
-declare -A ruleprefix31=(
-  [0x240b0010]=106,72
-  [0x240b0012]=14,8
-  [0x240b0250]=14,10
-  [0x240b0252]=14,12
-  [0x24047a80]=133,200
-  [0x24047a84]=133,206
-)
-
-# WANのIPv6アドレスを取得する関数
-getWANIPv6() {
-    local wan_ipv6
-    network_get_ipaddr6 wan_ipv6 wan
-    echo $wan_ipv6
-}
+# network.shの関数を利用するためにsourceする
+source ./lib/function/network.sh
 
 # IPv6アドレスの先頭32ビットを0x形式で取得し、対応するIPv4プレフィックスを返す関数
 getIPv4Prefix() {
     local ipv6=$1
+
+    # IPv6アドレスの先頭32ビットを0x形式で取得
     local prefix=$(echo $ipv6 | sed -E 's/([0-9a-fA-F]{1,4}):([0-9a-fA-F]{1,4}).*/0x\1\2/')
-    local ipv4_prefix=${ruleprefix31[$prefix]}
-    if [ -n "$ipv4_prefix" ]; then
-        IFS=',' read -r ip1 ip2 <<< "$ipv4_prefix"
-        echo "$ip1.$ip2.0.0"
-    else
-        echo "対応するIPv4プレフィックスが見つかりません。"
-    fi
+
+    # 対応するIPv4プレフィックスを定義
+    declare -A ruleprefix31=(
+      [0x240b0010]=106.72.0.0
+      [0x240b0012]=14.8.0.0
+      [0x240b0250]=14.10.0.0
+      [0x240b0252]=14.12.0.0
+      [0x24047a80]=133.200.0.0
+      [0x24047a84]=133.206.0.0
+    )
+
+    echo "${ruleprefix31[$prefix]}"
 }
 
-# WANのIPv6アドレスを取得
-ipv6=$(getWANIPv6)
+# NET_ADDR6変数にWANのIPv6アドレスを格納
+network_flush_cache
+network_find_wan6 NET_IF6
+network_get_ipaddr6 NET_ADDR6 wan6"
 
 # IPv4プレフィックスを取得
-ipv4Prefix=$(getIPv4Prefix $ipv6)
-echo "IPv4 Prefix: $ipv4Prefix"
+ipv4Prefix=$(getIPv4Prefix wan6)
+
+if [ -n "$ipv4Prefix" ]; then
+    echo "IPv4 Prefix: $ipv4Prefix"
+else
+    echo "対応するIPv4プレフィックスが見つかりません。"
+fi

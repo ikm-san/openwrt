0f029f94811909bec4d93daef96eb5d34195ff62
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-02 15:51:02 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -740,13 +740,8 @@ local function find_ipv4_prefix(wan_ipv6)
 
         -- 有効なプレフィックス長を判断
         if ruleprefix38[hex_prefix_40] or ruleprefix38_20[hex_prefix_40] then
-                local section3_hex = wan_ipv6:sub(11, 12) -- 第3セクションの最初の2桁
-                ipv6_prefixlen = string.len(string.format("%b", tonumber(section3_hex, 16))) + 32
-                local third_octet = ipv4_prefix:match("^%d+%.%d+%.(%d+)") -- 第3セクションを抽出
-                local binary = string.format("%b", tonumber(third_octet)) -- 2進数に変換
-                ipv4_prefixlen = select(2, binary:gsub("0", "")):len() + 16 -- 1のビット数をカウントし、16を加える
 
-                -- ipv6prefixを32ビットもしくは48ビットセクションまで抽出し48ビットフォーマットの場合は00で埋める
+                     -- ipv6prefixを32ビットもしくは48ビットセクションまで抽出し48ビットフォーマットの場合は00で埋める
                      local function extract_ipv6_prefix(wan_ipv6)
                                     -- IPv6アドレスを":"で分割
                                     local ipv6_sections = {}
@@ -773,7 +768,15 @@ local function find_ipv4_prefix(wan_ipv6)
                         -- 最後にプレフィックスの省略形を生成
                         return prefix .. "::"
                     end
+            
                 ipv6_prefix = extract_ipv6_prefix(wan_ipv6)
+                local section3_hex = wan_ipv6:sub(11, 12) -- 第3セクションの最初の2桁
+                ipv6_prefixlen = string.len(string.format("%b", tonumber(section3_hex, 16))) + 32
+                local third_octet = ipv4_prefix:match("^%d+%.%d+%.(%d+)") -- 第3セクションを抽出
+                local binary = string.format("%b", tonumber(third_octet)) -- 2進数に変換
+                ipv4_prefixlen = select(2, binary:gsub("0", "")):len() + 16 -- 1のビット数をカウントし、16を加える
+
+
             
         elseif ruleprefix31[hex_prefix_32] then
             ipv6_prefixlen = '32'

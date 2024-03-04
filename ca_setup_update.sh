e3165d8eaf1083df3231641951cd88defa353603
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-04 17:45:26 +0900

    Update ca_setup_update.sh

--- ca_setup_update.sh
+++ ca_setup_update.sh
@@ -8,15 +8,14 @@ opkg install lua luci-base
 opkg install luci-lua-runtime
 opkg install liblucihttp-lua
 opkg install luci-app-uhttpd
+opkg install luci-proto-ipv6 
+opkg install mape
+opkg install ds-lite 
 
 #CAセットアップメニュー用のファイル、キャッシュのクリア、Luciサービス再起動
 wget -O /usr/lib/lua/luci/controller/ca_setup.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua
 wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua
+wget -O /usr/lib/lua/luci/model/cbi/ca_setup/sandbox.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/sandbox.lua
 wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup
-chmod +x /usr/lib/lua/luci/controller/ca_setup.lua
-chmod +x /usr/lib/lua/luci/model/cbi/ca_setup/*.lua
 rm -rf /tmp/luci-*
 /etc/init.d/uhttpd restart
-
-
-chmod +x /usr/bin/restore_network.sh

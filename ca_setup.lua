f12c3fb90fc195c57597b2817e9c3d026e40c450
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-04 10:27:08 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -6,9 +6,6 @@ function index()
     
     -- IPoE設定タブ
     entry({"admin", "ca_setup", "ipoe"}, cbi("ca_setup/ipoe"), _("Internet接続設定"), 10)
-    
-    -- その他接続設定タブ
-    entry({"admin", "ca_setup", "other"}, cbi("ca_setup/others"), _("設定バックアップ"), 50)
 
     -- デバッグ用
     entry({"admin", "ca_setup", "sandbox"}, cbi("ca_setup/sandbox"), _("テスト用"), 60)

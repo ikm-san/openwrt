354d08427b166b55b8b9f2672b13c20987cf7b21
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-05 11:09:09 +0900

    Update ca_setup_update.sh

--- ca_setup_update.sh
+++ ca_setup_update.sh
@@ -2,15 +2,23 @@
 #network configファイルのバックアップを取る
 cp /etc/config/network /etc/config/network.old
 
+# Interface - LAN を192.168.10.1に変更してほかのルーター下に置けるようにする
+
 #不足している場合に備え、必要なソフトウェアのインストール
+#CAセットアップメニュー用のファイル、キャッシュのクリア、Luciサービス再起動
+# 
+
+
+Clear-Content .ssh\known_hosts -Force
+
+
+
 opkg update
 opkg install luci-lua-runtime
-opkg install luci-app-uhttpd
-opkg install luci-proto-ipv6 
+opkg install luci-proto-ipv6
+opkg install luci-compat
 opkg install map
 opkg install ds-lite 
-
-#CAセットアップメニュー用のファイル、キャッシュのクリア、Luciサービス再起動
 mkdir -p /usr/lib/lua/luci/controller/
 mkdir -p /usr/lib/lua/luci/model/cbi/ca_setup/
 wget -O /usr/lib/lua/luci/controller/ca_setup.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua
@@ -20,9 +28,13 @@ wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/m
 rm -rf /tmp/luci-*
 /etc/init.d/uhttpd restart
 
+reboot
+
 ?
 opkg install lua luci-base
 opkg install liblucihttp-lua
 
-Clear-Content .ssh\known_hosts -Force
+opkg install luci-compat
+opkg install luci-app-uhttpd
+
 

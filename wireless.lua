e4c4666105ebf2ec8f3fbe43e2d1f696213e4567
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-05 13:15:27 +0900

    Update wireless.lua

--- wireless.lua
+++ wireless.lua
@@ -1,7 +1,7 @@
 local uci = require "luci.model.uci".cursor()
 local sys = require "luci.sys"
 
-m = Map("wireless", _("Wireless Configuration"))
+m = Map("wireless", "WiFi各種設定))
 m.apply_on_parse = true -- SAVE&APPLYボタンが押されたときに設定を適用
 
 s = m:section(TypedSection, "wifi-iface", _("Settings"))
@@ -17,26 +17,27 @@ password.datatype = "pw"
 password.password = true
 
 function m.on_commit(map)
-    local devices = {"radio0", "radio1"} -- 2.4GHzと5GHzのデバイス名を指定
+    local devices = {"radio0", "radio1", "radio2"} -- 2.4GHz, 5GHz, および 6GHzのデバイス名を指定
 
     for _, dev in ipairs(devices) do
-        -- 国コードと最大送信電力の設定
-        uci:set("wireless", dev, "country", "JP")
-        uci:set("wireless", dev, "txpower", "10")
+        -- デバイスの存在を確認
+        if uci:get("wireless", dev) ~= nil then
+            -- 国コードと最大送信電力の設定
+            uci:set("wireless", dev, "country", "JP")
+            uci:set("wireless", dev, "txpower", "10")
 
-        -- インターフェースの設定
-        local iface_section = dev .. "_network"
-        uci:set("wireless", iface_section, "device", dev)
-        uci:set("wireless", iface_section, "mode", "ap")
-        uci:set("wireless", iface_section, "ssid", ssid:formvalue(dev))
-        uci:set("wireless", iface_section, "encryption", "psk2+ccmp")
-        uci:set("wireless", iface_section, "key", password:formvalue(dev))
-        uci:set("wireless", iface_section, "disabled", "0") -- Enable wireless
+            -- インターフェースの設定
+            local iface_section = dev .. "_network"
+            uci:set("wireless", iface_section, "device", dev)
+            uci:set("wireless", iface_section, "mode", "ap")
+            uci:set("wireless", iface_section, "ssid", ssid:formvalue(dev))
+            uci:set("wireless", iface_section, "encryption", "psk2+ccmp")
+            uci:set("wireless", iface_section, "key", password:formvalue(dev))
+            uci:set("wireless", iface_section, "disabled", "0") -- Enable wireless
+        end
     end
 
     -- 設定の保存と適用
     uci:commit("wireless")
     sys.call("wifi reload")
 end
-
-return m

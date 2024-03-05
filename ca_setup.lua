a7d85a4ecefaa9a7ab7895e6279d82e3317118a2
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-05 16:58:50 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -11,7 +11,7 @@ function index()
     entry({"admin", "ca_setup", "wireless"}, cbi("ca_setup/wireless"), _("WiFi接続設定"), 20)
 
     -- アップデート処理
-    entry({"admin", "ca_setup", "update"}, cbi("ca_setup/update"), _("設定ソフトの更新処理"), 20)
+    entry({"admin", "ca_setup", "update"}, cbi("ca_setup/update"), _("最新版へ更新処理"), 30)
     
     -- デバッグ用
     entry({"admin", "ca_setup", "sandbox"}, cbi("ca_setup/sandbox"), _("テスト用"), 60)

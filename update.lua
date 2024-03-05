d295f4bcd9ef958bd7a54c7baa9c6e61ff504cce
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-05 16:53:40 +0900

    Create update.lua

---
+++ update.lua
@@ -0,0 +1,28 @@
+local fs = require "nixio.fs"
+local sys = require "luci.sys"
+
+m = Map("ca_setup", translate("CA Setup"),
+        translate("This page allows you to update CA setup files."))
+
+s = m:section(TypedSection, "ca_setup", "Files")
+s.anonymous = true
+
+function s.cfgsections()
+    return { "_update" }
+end
+
+update = s:option(Button, "_update", "Update Files")
+update.inputtitle = translate("Update Now")
+update.inputstyle = "reload"
+
+function update.write()
+    sys.exec("wget -O /usr/lib/lua/luci/controller/ca_setup.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua")
+    sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua")
+    sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/wireless.lua")
+    sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/sandbox.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/sandbox.lua")
+    sys.exec("wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup")
+    sys.exec("rm -rf /tmp/luci-*")
+    sys.exec("/etc/init.d/uhttpd restart")
+end
+
+return m

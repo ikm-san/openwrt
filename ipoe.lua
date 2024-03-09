9e8f66aa9b46edaa8cea13f45bd475baf55c2666
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-09 15:08:08 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -1,7 +1,7 @@
 local fs = require "nixio.fs"
 local sys = require "luci.sys"
 local uci = require "luci.model.uci".cursor()
--- local ca_function = require "ca_function" 
+local calib = require "calib" 
 
 
 -- basic map-e conversion table based on http://ipv4.web.fc2.com/map-e.html
@@ -715,21 +715,8 @@ local function dec_to_bin(dec)
     return bin == "" and "0" or bin
 end
 
--- WANインターフェースのIPv6アドレス（scope global）を取得
-local function get_wan_ipv6_global()
-    local command = "ip -6 addr show dev wan | awk '/inet6/ && /scope global/ {print $2}' | cut -d'/' -f1 | head -n 1"
-    local ipv6_global = sys.exec(command)
-    local normalized_ipv6 = ipv6_global:match("([a-fA-F0-9:]+)") -- IPv6アドレスの正規化
 
-    -- IPv6アドレスが見つからない場合は0を返す
-    if normalized_ipv6 == nil or normalized_ipv6 == '' then
-        return '0'
-    else
-        return normalized_ipv6
-    end
-end
--- local wan_ipv6 = {}
-local wan_ipv6 = get_wan_ipv6_global() -- WANのグローバルIPv6を取得
+local wan_ipv6 = calib.get_wan_ipv6_global() -- WANのグローバルIPv6を取得
 
 
 -- Pattern to match the first four sections of an IPv6 address

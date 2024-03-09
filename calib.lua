bc3dff44c267cff435c0a6b5eef29e279c6f342f
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-09 15:26:31 +0900

    Update calib.lua

--- calib.lua
+++ calib.lua
@@ -39,4 +39,17 @@ function M.fetchRules()
     return map_rule
 end
 
+-- basic map-e conversion table based on http://ipv4.web.fc2.com/map-e.html
+function M.getRulePrefix31()
+    local ruleprefix31 = {
+        ["240b0010"] = "106.72",
+        ["240b0012"] = "14.8",
+        ["240b0250"] = "14.10",
+        ["240b0252"] = "14.12",
+        ["24047a80"] = "133.200",
+        ["24047a84"] = "133.206"
+    }
+    return ruleprefix31
+end
+
 return M

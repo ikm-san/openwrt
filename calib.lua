190cdcf643a56c3d005d823955765e969655ae4b
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-09 16:18:11 +0900

    Update calib.lua

--- calib.lua
+++ calib.lua
@@ -54,8 +54,8 @@ end
 
 -- Mape関連の数値を取得する関数、IPv6アドレスから対応するIPv4プレフィックスを取得
 function M.find_ipv4_prefix()
-    local segments = {}
     local wan_ipv6 = M.get_wan_ipv6_global
+    local segments = {}
     for seg in wan_ipv6:gmatch("[a-fA-F0-9]+") do
         table.insert(segments, string.format("%04x", tonumber(seg, 16)))
     end

b3e8e9b91fdf59dc66b06443ecc2f7d84b83a8d7
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-10 17:13:57 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -4,7 +4,7 @@
 url="https://api.enabler.ne.jp/6823228689437e773f260662947d6239/get_rules"
 
 # curlを使用してデータを取得
-response=$(curl -s "$url")
+map_rules=$(curl -s "$url")
 
 echo "取得したデータ:"
 echo "$map_rules"

dda02d518c670a39d3c91407ac70fb534e795fd7
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-10 17:08:48 +0900

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -6,17 +6,5 @@ url="https://api.enabler.ne.jp/6823228689437e773f260662947d6239/get_rules"
 # curlを使用してデータを取得
 response=$(curl -s "$url")
 
-# レスポンスの確認
-if [ -z "$response" ]; then
-    echo "データの取得に失敗しました。"
-else
-    # JSONP応答からJSON部分のみを抽出
-    map_rules=$(echo $response | sed -n 's/^?(\(.*\))$/\1/p')
-    
-    if [ -z "$map_rules" ]; then
-        echo "JSONPからJSONを抽出できませんでした。"
-    else
-        echo "データの取得に成功しました:"
-        echo "$map_rules"
-    fi
-fi
+echo "取得したデータ:"
+echo "$map_rules"

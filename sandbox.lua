d764c4458798e3f4e3cea69649d22d3ab4287785
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-10 15:55:33 +0900

    Update sandbox.lua

--- sandbox.lua
+++ sandbox.lua
@@ -1,238 +1,41 @@
 local fs = require "nixio.fs"
-local sys = require "luci.sys"
-local uci = require "luci.model.uci".cursor()
+local http = require "luci.http"
+local jsonc = require "luci.jsonc"
 
--- IPv6からIPv4プレフックスへの変換マップ
-local ruleprefix31 = {
-    ["240b0010"] = "106.72",
-    ["240b0012"] = "14.8",
-    ["240b0250"] = "14.10",
-    ["240b0252"] = "14.12",
-    ["24047a80"] = "133.200",
-    ["24047a84"] = "133.206"
-}
+local m, s, btn
 
-local ruleprefix38 = {
-    ["24047a8200"] = "125.196.208",
-    ["24047a8204"] = "125.196.212",
-    ["24047a8208"] = "125.198.140",
-    ["24047a820c"] = "125.198.144",
-    ["24047a8210"] = "125.198.212"
-}
+m = SimpleForm("fetchdata", "Fetch Data")
+m.reset = false
+m.submit = false
 
-local ruleprefix38_20 = {
-    ["2400405000"] = "153.240.0",
-    ["2400405004"] = "153.240.16",
-    ["2400405008"] = "153.240.32",
-    ["240040500c"] = "153.240.48",
-    ["2400405010"] = "153.240.64"
-}
-
--- WANインターフェースのIPv6アドレス（scope global）を取得
-local function get_wan_ipv6_global()
-    local command = "ip -6 addr show dev wan | awk '/inet6/ && /scope global/ {print $2}' | cut -d'/' -f1 | head -n 1"
-    local ipv6_global = sys.exec(command)
-    return ipv6_global:match("([a-fA-F0-9:]+)") -- IPv6アドレスの正規化
+function fetch_data()
+    local url = "https://api.example.com/data"
+    local response, code = http.request(url)
+    if code ~= 200 then
+        return nil, "HTTP request failed"
+    end
+    
+    local jsonStr = response:match("%((.+)%)") -- JSONP response parsing
+    if not jsonStr then
+        return nil, "Failed to extract JSON"
+    end
+    
+    local status, data = pcall(jsonc.parse, jsonStr)
+    if not status then
+        return nil, "JSON parsing failed"
+    end
+    
+    return data, nil
 end
 
-local wan_ipv6 = get_wan_ipv6_global()
-
--- IPv6アドレスから対応するIPv4プレフィックスを取得
-local function find_ipv4_prefix(wan_ipv6)
-    local segments = {}
-    for seg in wan_ipv6:gmatch("[a-fA-F0-9]+") do
-        table.insert(segments, string.format("%04x", tonumber(seg, 16)))
-    end
-
-    local full_ipv6 = table.concat(segments, ":"):gsub("::", function(s)
-        return ":" .. string.rep("0000:", 8 - #segments)
-    end)
-
-    -- 40ビットと32ビットのプレフィックスを取得
-    local hex_prefix_40 = full_ipv6:gsub(":", ""):sub(1, 10)
-    local hex_prefix_32 = full_ipv6:gsub(":", ""):sub(1, 8)
-
-    local ipv4_prefix = ruleprefix38[hex_prefix_40] or ruleprefix38_20[hex_prefix_40] or ruleprefix31[hex_prefix_32]
-
-    if ipv4_prefix then
-        local ipv4_parts = {}
-        for part in ipv4_prefix:gmatch("(%d+)") do
-            table.insert(ipv4_parts, part)
-        end
-        while #ipv4_parts < 4 do
-            table.insert(ipv4_parts, "0")
-        end
-        return table.concat(ipv4_parts, ".") -- 例: "106.72.0.0"
+btn = m:field(Button, "fetch", "Fetch Data", "Click to fetch data")
+btn.inputstyle = "reload"
+function btn.write()
+    local data, err = fetch_data()
+    if data then
+        m.message = "Data fetched successfully: " .. jsonc.stringify(data)
     else
-        return nil, "No matching IPv4 prefix found."
-    end
-end
-
-
-m = Map("ca_setup", "WAN接続設定", "下記のリストより適切なものを選んで実行してください。")
-
-s = m:section(TypedSection, "ipoe", "")
-s.addremove = false
-s.anonymous = true
-
-choice = s:option(ListValue, "wan_setup", "操作")
-choice:value("dhcp_auto", "DHCP自動")
-choice:value("pppoe_ipv4", "PPPoE接続")
-choice:value("ipoe_v6plus", "v6プラス")
-choice:value("ipoe_ocnvirtualconnect", "OCNバーチャルコネクト")
-choice:value("ipoe_biglobe_e", "IPv6オプション EAST")
-choice:value("ipoe_biglobe_w", "IPv6オプション WEST")
-choice:value("ipoe_transix", "transix")
-choice:value("ipoe_xpass", "クロスパス")
-choice:value("ipoe_v6connect", "v6コネクト")
-choice:value("bridge_mode", "ブリッジモード")
-
--- PPPoEユーザー名とパスワード入力フォームの追加及び、選択された場合のみ、ユーザー名とパスワード欄を表示
-username = s:option(Value, "username", "PPPoE ユーザー名")
-password = s:option(Value, "password", "PPPoE パスワード")
-password.password = true
-username:depends("wan_setup", "pppoe_ipv4")
-password:depends("wan_setup", "pppoe_ipv4")
-
--- インターフェース設定を削除する関数
-local function deleteInterfaces()
-    local interfaces = {"wan", "wan6", "wanmap", "pppoe_wan", "ds-lite", "map-e"}
-    for _, interface in ipairs(interfaces) do
-        uci:delete("network", interface)
-    end
-end
-
--- ds-lite接続設定関数
-local function configure_dslite_connection(gw_aftr)
-    -- DHCP LAN設定
-    uci:set("dhcp", "lan", "dhcp")
-    uci:set("dhcp", "lan", "ra", "relay")
-    uci:set("dhcp", "lan", "dhcpv6", "server")
-    uci:set("dhcp", "lan", "ndp", "relay")
-    uci:set("dhcp", "lan", "force", "1")
-
-    -- WAN設定の無効化
-    uci:set("network", "wan", "auto", "0")
-
-    -- DS-Liteインターフェースの設定
-    uci:section("network", "interface", "dslite", {
-        proto = 'dslite',
-        peeraddr = gw_aftr, 
-        tunlink = 'wan6',
-        mtu = '1460'
-    })
-
-    -- DHCP関連設定
-    uci:set("dhcp", "wan6", "dhcp")
-    uci:set("dhcp", "wan6", "interface", "wan6")
-    uci:set("dhcp", "wan6", "master", "1")
-    uci:set("dhcp", "wan6", "ignore", "1")
-    uci:set("dhcp", "wan6", "dhcpv6", "relay")
-    uci:set("dhcp", "wan6", "ra", "relay")
-    uci:set("dhcp", "wan6", "ndp", "relay")
-
-    -- DS-LiteインターフェースをWANゾーンに追加
-    uci:add_list("firewall", "@zone[1]", "network", "dslite")
-
-    -- 設定のコミット
-    uci:commit("dhcp")
-    uci:commit("network")
-    uci:commit("firewall")
-
-    -- ネットワークサービス、DHCPサービス、ファイアウォールの再起動
-    os.execute("/etc/init.d/network restart")
-    os.execute("/etc/init.d/dnsmasq restart")
-    os.execute("/etc/init.d/firewall restart")
-end
-
-
-
---デバッグ表示用
-        local ipv4_prefix = find_ipv4_prefix(wan_ipv6)
-
-            o = s:option(DummyValue, "wan_ipv6", translate("WAN IPv6 Address"))
-            o.value = wan_ipv6 or translate("Not available")
-            
-            o = s:option(DummyValue, "ipv4_prefix", translate("MAPE IPv4 Prefix"))
-            o.value = ipv4_prefix or translate("No matching IPv4 prefix found.")
---ここまで
-
-
-
--- LuciのSAVE＆APPLYボタンが押された時の動作
-function m.on_commit(map)
-    local choice_val = m.uci:get("ca_setup", "ipoe", "wan_setup")
-    local gw_aftr = m.uci:get("ca_setup", choice_val, "gw_aftr")
-    --既存のWAN設定を削除
-    deleteInterfaces()
-    
-    if choice_val == "dhcp_auto" then
-
-        -- DHCP自動設定を適用
-        uci:set("network", "wan", "interface")
-        uci:set("network", "wan", "proto", "dhcp")
-        uci:set("network", "wan6", "interface")
-        uci:set("network", "wan6", "proto", "dhcpv6")
-        uci:set("network", "wan6", "reqaddress", "try")
-        uci:set("network", "wan6", "reqprefix", "auto")
-        uci:commit("network")
-        luci.sys.exec("/etc/init.d/network restart")
-        
-    elseif choice_val == "pppoe_ipv4" then
-        
-        -- PPPoE設定を適用
-        local user = m.uci:get("ca_setup", "ipoe", "username")
-        local pass = m.uci:get("ca_setup", "ipoe", "password")
-        uci:set("network", "pppoe_wan", "interface")
-        uci:set("network", "pppoe_wan", "proto", "pppoe")
-        uci:set("network", "pppoe_wan", "username", user)
-        uci:set("network", "pppoe_wan", "password", pass)
-        uci:set("network", "pppoe_wan", "ifname", "eth0.2")
-        uci:commit("network")
-        luci.sys.exec("/etc/init.d/network restart")
-
-    elseif choice_val == "ipoe_v6plus" then
-       
-        -- v6プラス
-
-        -- ここにいれる
-    
-    elseif choice_val == "ipoe_ocnvirtualconnect" then
-        
-        -- OCNバーチャルコネクト
-        -- ここにいれる
-
-    elseif choice_val == "ipoe_biglobe_e" then
-        
-        -- BIGLOBE IPv6オプション
-        -- ここにいれる
-
-        
-    elseif choice_val == "ipoe_biglobe_w" then
-        
-        -- BIGLOBE IPv6オプション
-        -- ここにいれる
-
-        
-    elseif choice_val == "ipoe_transix" then
-        -- transix (ds-lite)
-           configure_dslite_connection(gw_aftr)
-    
-    elseif choice_val == "ipoe_xpass" then
-        -- クロスパス (ds-lite)
-           configure_dslite_connection(gw_aftr)
-        
-    elseif choice_val == "ipoe_v6connect" then
-        -- v6コネクト
-           configure_dslite_connection(gw_aftr)
-        
-    elseif choice_val == "bridge_mode" then
-        -- ブリッジモード設定の適用
-        -- uci:set("network", "lan", "type", "bridge")
-        -- uci:set("network", "lan", "ifname", "eth0.1 eth0.2")  -- 例としてeth0.1とeth0.2をブリッジ
-        -- uci:delete("network", "lan", "proto")  -- DHCPなどの既存設定を削除
-        -- uci:commit("network")
-        -- luci.sys.exec("/etc/init.d/network restart")
+        m.message = "Error fetching data: " .. err
     end
 end
 

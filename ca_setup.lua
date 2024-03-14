6c4ea97184ce1748e1f3cd6b3d26ccb9f445a803
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-14 22:43:38 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -2,7 +2,7 @@ module("luci.controller.ca_setup", package.seeall)
 
 function index()
     -- CA SETUPのトップレベルエントリを作成
-    entry({"admin", "ca_setup"}, firstchild(), _("CA SETUP"), 60).dependent = false
+    entry({"admin", "ca_setup"}, firstchild(), _("CA接続設定"), 60).dependent = false
     
     -- IPoE設定タブ
     entry({"admin", "ca_setup", "ipoe"}, cbi("ca_setup/ipoe"), _("Internet接続設定"), 10)

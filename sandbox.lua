c75dbe779a3329acc1d051269a798af54b70d5f5
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-15 16:56:53 +0900

    Update sandbox.lua

--- sandbox.lua
+++ sandbox.lua
@@ -11,17 +11,46 @@ m.submit = false
 s = m:section(SimpleSection, translate("Settings"))
 
 -- WANのグローバルIPv6を取得
-wan_ipv6 = calib.get_wan_ipv6_global()
-local wan32_ipv6, wan40_ipv6 = calib.wan32_40(wan_ipv6)
+-- wan_ipv6 = calib.get_wan_ipv6_global()
+-- local wan32_ipv6, wan40_ipv6 = calib.wan32_40(wan_ipv6)
 
-local peeraddr = uci:get("ca_setup", "map", "dmr")
-local ipv6_fixlen = uci:get("ca_setup", "map", "ipv6_fixlen")
+-- local peeraddr = uci:get("ca_setup", "map", "dmr")
+-- local ipv6_fixlen = uci:get("ca_setup", "map", "ipv6_fixlen")
 
 -- fmrの読み込みと解析
-local fmr_json = uci:get("ca_setup", "map", "fmr")
-local fmr = jsonc.parse(fmr_json)
+-- local fmr_json = uci:get("ca_setup", "map", "fmr")
+-- local fmr = jsonc.parse(fmr_json)
 
-local peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset = calib.get_mapconfig(fmr)
+
+-- map configを出力する関数 --
+function get_mapconfig()
+    local wan_ipv6 = calib.get_wan_ipv6_global()
+    local sections = calib.split_ipv6(wan_ipv6)
+    local wan32_ipv6, wan40_ipv6 = calib.wan32_40(wan_ipv6)
+    local peeraddr = uci:get("ca_setup", "map", "dmr")
+    local ipv6_fixlen = uci:get("ca_setup", "map", "ipv6_fixlen")
+    local fmr_json = uci:get("ca_setup", "map", "fmr")
+    local fmr = jsonc.parse(fmr_json)
+    local matching_fmr = calib.find_matching_fmr(wan40_ipv6, fmr) or calib.find_matching_fmr(wan32_ipv6, fmr)
+
+    if matching_fmr then
+        local ipv6_prefix, ipv6_prefix_length = matching_fmr.ipv6:match("^(.-)/(%d+)$")
+        local ipv4_prefix, ipv4_prefix_length = matching_fmr.ipv4:match("^(.-)/(%d+)$")
+        local ealen = matching_fmr.ea_length
+        local offset = matching_fmr.psid_offset
+        local psidlen = ealen - (32 - ipv4_prefix_lenth)
+        return peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset
+    else
+        error("No matching FMR entry found.")
+    end
+end
+
+
+local peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset = get_mapconfig()
+
+
+
+
 -- wan_ipv6アドレスにマッチするfmrエントリを検索する関数
 --local function find_matching_fmr(wan_ipv6, fmr_list)
 --    for _, entry in ipairs(fmr_list) do

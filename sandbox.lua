9a82f43a0da7c3646a34c7dd54786346a93c14ac
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-16 15:38:56 +0900

    Update sandbox.lua

--- sandbox.lua
+++ sandbox.lua
@@ -12,31 +12,7 @@ s = m:section(SimpleSection, translate("Settings"))
 
 local wan_ipv6 = calib.get_wan_ipv6_global()
 
--- map configを出力する関数 --
-function get_mapconfig(wan_ipv6)
-    local sections = calib.split_ipv6(wan_ipv6)
-    local wan32_ipv6, wan40_ipv6 = calib.wan32_40(wan_ipv6)
-    local ipv6_56 = calib.extract_ipv6_56(wan_ipv6)
-    local peeraddr = uci:get("ca_setup", "map", "dmr")
-    local ipv6_fixlen = uci:get("ca_setup", "map", "ipv6_fixlen")
-    local fmr_json = uci:get("ca_setup", "map", "fmr")
-    local fmr = jsonc.parse(fmr_json)
-    local matching_fmr = calib.find_matching_fmr(wan40_ipv6, fmr) or calib.find_matching_fmr(wan32_ipv6, fmr)
-
-    if matching_fmr then
-        local ipv6_prefix, ipv6_prefix_length = matching_fmr.ipv6:match("^(.-)/(%d+)$")
-        local ipv4_prefix, ipv4_prefix_length = matching_fmr.ipv4:match("^(.-)/(%d+)$")
-        local ealen = matching_fmr.ea_length
-        local offset = matching_fmr.psid_offset
-        local psidlen = ealen - (32 - ipv4_prefix_length)
-        return peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset, ipv6_fixlen, ipv6_56, fmr, fmr_json, wan_ipv6, wan32_ipv6, wan40_ipv6
-    else
-        error("No matching FMR entry found.")
-    end
-end
-
-
-local peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset, ipv6_fixlen, ipv6_56, fmr, fmr_json, wan_ipv6, wan32_ipv6, wan40_ipv6 = get_mapconfig(wan_ipv6)
+local peeraddr, ipv4_prefix, ipv4_prefix_length, ipv6_prefix, ipv6_prefix_length, ealen, psidlen, offset, ipv6_fixlen, ipv6_56, fmr, fmr_json, wan_ipv6, wan32_ipv6, wan40_ipv6 = calib.get_mapconfig(wan_ipv6)
 
 
     s:option(DummyValue, "_wan_ipv6", translate("wan_ipv6")).value = wan_ipv6

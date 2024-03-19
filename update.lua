575b8c473ce23822eec87e85af35e7313e463519
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-19 10:15:23 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -25,6 +25,8 @@ function update.write()
         sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/sandbox.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/sandbox.lua")
         sys.exec("wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup")
         sys.exec("rm -rf /tmp/luci-*")
+
+        http.write("<script>alert('更新が完了しました。再起動します。');</script>")
         sys.exec("/etc/init.d/uhttpd restart")
         -- デバイスを再起動する
         -- luci.sys.reboot()

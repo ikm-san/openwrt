f9a9740cf8ed1be6de06c9b383fa73368239b4f8
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-19 20:34:06 +0900

    Update update.lua

--- update.lua
+++ update.lua
@@ -12,7 +12,7 @@ function s.cfgsections()
     return { "_update" }
 end
 
-update = s:option(Button, "_update", "Update Files")
+update = s:option(Button, "_update", "ソフトウェアを更新")
 update.inputtitle = translate("Update Now")
 update.inputstyle = "reload"
 
@@ -22,11 +22,10 @@ function update.write()
         sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua")
         sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/wireless.lua")
         sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/update.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/update.lua")
-        sys.exec("wget -O /usr/lib/lua/luci/model/cbi/ca_setup/sandbox.lua https://raw.githubusercontent.com/ikm-san/openwrt/main/sandbox.lua")
         sys.exec("wget -O /etc/config/ca_setup https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup")
         sys.exec("rm -rf /tmp/luci-*")
 
-        http.write("<script>alert('更新が完了しました。再起動します。');</script>")
+        http.write("<script>alert('ソフトウェア更新が完了しました。再起動します。');</script>")
         sys.exec("/etc/init.d/uhttpd restart")
         -- デバイスを再起動する
         -- luci.sys.reboot()

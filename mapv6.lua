f7c5e54475e73ac570bc4c1375e32c67b599c0f4
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-19 14:57:54 +0900

    Update mapv6.lua

--- mapv6.lua
+++ mapv6.lua
@@ -81,7 +81,7 @@ if not conn then
     error("Failed to connect to ubus")
 end
 local system_info = conn:call("system", "board", {})
-local ukey = system_info.model:sub(1, 7) 
+
 local brand
 if system_info.model and string.find(system_info.model, "Linksys") then
     brandcheck = "OK"
@@ -109,6 +109,26 @@ function extract_ipv6_56(wan_ipv6)
 end
 
 
+-- 前回のIPv6 56アドレスと違いがないかチェック --
+function samewancheck(ipv6_56)
+    local last_ipv6_56 = uci:get("ca_setup", "map", "ipv6_56")
+    local samewan
+
+    if last_ipv6_56 == nil then
+        samewan = "N"
+    else
+        if last_ipv6_56 == ipv6_56 then
+            samewan = "Y"
+        else
+            samewan = "N"
+        end
+    end
+
+    return samewan
+end
+
+local ipv6_56 = extract_ipv6_56(wan_ipv6)
+local samewancheck = samewancheck(ipv6_56)
 
 
 -- mapルール確認回数のカウント --
@@ -152,7 +172,8 @@ local function decryptedData()
             return string.char(tonumber(cc, 16))
         end))
     end
-    local key = openssl.digest.digest("sha256", ukey, true)
+    local key = "Linksys"
+    local key = openssl.digest.digest("sha256", key, true)
     local encryptedData = hex_to_binary(hexEncryptedData)
     local cipher = openssl.cipher.get("aes-256-cbc")
     local decryptedData, err = cipher:decrypt(encryptedData, key)
@@ -186,7 +207,7 @@ function save_ca_setup_config(json_data)
         ostime = os.time(),
         model = system_info.model,
         VNE = VNE,
-        ipv4_prefix = ipv4_prefix,
+        ipv6_56 = ipv6_56,
         mapcount = mapcount
     })
     uci:commit("ca_setup")
@@ -273,30 +294,6 @@ function get_mapconfig(wan_ipv6)
     end
 end
 
-local peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_fixlen, ipv6_56, fmr, fmr_json, wan_ipv6, wan32_ipv6, wan40_ipv6 = get_mapconfig(wan_ipv6)
-
--- 前回のIPv4 mapアドレスと違いがないかチェック --
-function sameipv4mapcheck(ipv4_prefix)
-    local last_ipv4_prefix = uci:get("ca_setup", "map", "ipv4_prefix")
-    local sameipv4mapcheck
-
-    if last_ipv4_prefix == nil then
-        sameipv4mapcheck = "N"
-    else
-        if last_ipv4_prefix == ipv4_prefix then
-            sameipv4mapcheck = "Y"
-        else
-            sameipv4mapcheck = "N"
-        end
-    end
-
-    return sameipv4mapcheck
-end
-
-local sameipv4mapcheck = sameipv4mapcheck(ipv4_prefix)
-
-
-
 -- map-e v6 plus 接続設定関数
 function configure_mape_connection(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
       
@@ -355,13 +352,13 @@ end
 -- ページ読み込み時にデータ取得を自動実行
 if reloadtimer == "Y" and brandcheck == "OK" and VNE == "v6プラス" then
     auto_fetch_data()
-        if sameipv4mapcheck == "N" then
-            print("map IPv4アドレスが前回起動時と違うので設定変更ルーチンが実行する想定")
-            
+        if samewancheck == "N" then
+            print("WANが前回起動時と違うので設定変更ルーチンが実行する想定")
+            local peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_fixlen, ipv6_56, fmr, fmr_json, wan_ipv6, wan32_ipv6, wan40_ipv6 = get_mapconfig(wan_ipv6)
             configure_mape_connection(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
             print("map設定を変更しました。10秒後にリブートします...")
-            -- os.execute("sleep 10") 
-            -- os.execute("reboot")
+            os.execute("sleep 10") 
+            os.execute("reboot")
         end
 else
     print("実行していません: " .. reloadtimer .. ", " .. brandcheck .. ", " .. VNE)

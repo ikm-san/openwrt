9c21b1d4f2eed33b735fa102aaf6390edea248a1
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-19 11:30:32 +0900

    Update sandbox.lua

--- sandbox.lua
+++ sandbox.lua
@@ -5,15 +5,14 @@ local key = "Linksys"
 -- 暗号化のためのキーを適切な長さに調整
 local key = openssl.digest.digest("sha256", key, true)
 
-local data = "bf502ae10c4b83e034891e62626c01d4b70f48e5e361eb75fcb4fc0d2fa774ea4a331c285cb59d9f5a11c46b0a0368ca1253283d891df54962778c225d79fd25ae9d688614ebef0a30e961e1153ad5ca"
+-- 暗号化されたデータ（16進数文字列）
+local hexEncryptedData = "bf502ae10c4b83e034891e62626c01d4b70f48e5e361eb75fcb4fc0d2fa774ea4a331c285cb59d9f5a11c46b0a0368ca1253283d891df54962778c225d79fd25ae9d688614ebef0a30e961e1153ad5ca"
 
--- 暗号化
--- local encryptedData, err = cipher:encrypt(data, key)
--- assert(encryptedData, err)
+-- 16進数の文字列をバイナリデータに変換
+local encryptedData = openssl.hex(hexEncryptedData)
 
--- 復号化
-local decryptedData, err = cipher:decrypt(data, key)
-assert(data, err)
+-- 復号化（IVが必要な場合は、適切にIVを指定する必要があります）
+local decryptedData, err = cipher:decrypt(encryptedData, key)
+assert(decryptedData, err)
 
--- print("暗号化されたデータ: ", openssl.hex(encryptedData))
 print("復号化されたデータ: ", decryptedData)

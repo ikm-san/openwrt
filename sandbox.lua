b90aa94f7bd3096b8451c055d026062f1849804f
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-19 10:37:31 +0900

    Update sandbox.lua

--- sandbox.lua
+++ sandbox.lua
@@ -5,15 +5,15 @@ local key = "Linksys"
 -- 暗号化のためのキーを適切な長さに調整
 local key = openssl.digest.digest("sha256", key, true)
 
-local data = "https://api.enabler.ne.jp/6823228689437e773f260662947d6239/get_rules"
+local data = "https://api.enabler.ne.jp/6823228689437e773f260662947d6239/get_rulesbf502ae10c4b83e034891e62626c01d4b70f48e5e361eb75fcb4fc0d2fa774ea4a331c285cb59d9f5a11c46b0a0368ca1253283d891df54962778c225d79fd25ae9d688614ebef0a30e961e1153ad5ca"
 
 -- 暗号化
-local encryptedData, err = cipher:encrypt(data, key)
-assert(encryptedData, err)
+-- local encryptedData, err = cipher:encrypt(data, key)
+-- assert(encryptedData, err)
 
 -- 復号化
-local decryptedData, err = cipher:decrypt(encryptedData, key)
-assert(decryptedData, err)
+local decryptedData, err = cipher:decrypt(data, key)
+assert(data, err)
 
-print("暗号化されたデータ: ", openssl.hex(encryptedData))
+-- print("暗号化されたデータ: ", openssl.hex(encryptedData))
 print("復号化されたデータ: ", decryptedData)

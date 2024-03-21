c7128ed7b5588f922ba11c875a478c99f5b62494
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-03-21 10:00:47 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -296,32 +296,32 @@ function choice.write(self, section, value)
         uci:set_list("firewall", "@zone[1]", "network", {"wan"})     
         uci:commit("firewall")
         
-    elseif value == "ipoe_v6plus" then      
+    elseif value == "ipoe_v6plus" and VNE == "v6プラス" then      
         -- v6プラス
             local ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56, peeraddr = calib.find_ipv4_prefix(wan_ipv6)
             configure_mape_connection(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
     
-    elseif value == "ipoe_ocnvirtualconnect" then
+    elseif value == "ipoe_ocnvirtualconnect" and VNE == "OCNバーチャルコネクト" then
         -- OCNバーチャルコネクト
             local ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56, peeraddr = calib.find_ipv4_prefix(wan_ipv6)
             configure_mape_ocn(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
         
-    elseif value == "ipoe_biglobe" then
+    elseif value == "ipoe_biglobe" and VNE == "IPv6オプション" then
         -- BIGLOBE IPv6オプション
             local ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56, peeraddr = calib.find_ipv4_prefix(wan_ipv6)
             configure_mape_connection(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
         
-    elseif value == "ipoe_transix" then
+    elseif value == "ipoe_transix and VNE == "transix" then
         -- transix (ds-lite)
             gw_aftr = m.uci:get("ca_setup", "ipoe_transix", "gw_aftr")
             configure_dslite_connection(gw_aftr)
     
-    elseif value == "ipoe_xpass" then
+    elseif value == "ipoe_xpass" and VNE == "クロスパス" then
         -- クロスパス (ds-lite)
             gw_aftr = m.uci:get("ca_setup", "ipoe_xpass", "gw_aftr")
             configure_dslite_connection(gw_aftr)
         
-    elseif value == "ipoe_v6connect" then
+    elseif value == "ipoe_v6connect" and VNE == "v6コネクト" then
         -- v6コネクト
             gw_aftr = m.uci:get("ca_setup", "ipoe_v6connect", "gw_aftr")
             configure_dslite_connection(gw_aftr)

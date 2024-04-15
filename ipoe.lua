4275a8a70044d518c7802fccefa039e9040c2057
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-04-16 03:17:16 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -242,8 +242,8 @@ end
 
 -- map-e v6 plus 接続設定関数
 function configure_mape_connection(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
--- local ipv6_56 = wan_ipv6
-local ip6prefix = ipv6_56 .. "/56"
+local ipv6_56 = wan_ipv6
+
               
     -- DHCP LAN settings
     uci:set("dhcp", "lan", "dhcp")
@@ -269,7 +269,7 @@ local ip6prefix = ipv6_56 .. "/56"
     uci:set("network", "wan6", "proto", "dhcpv6")
     uci:set("network", "wan6", "reqaddress", "try")
     uci:set("network", "wan6", "reqprefix", "auto")
-    uci:set("network", "wan6", "ip6prefix", ip6prefix)
+    uci:set("network", "wan6", "ip6prefix", ipv6_56 .. "/56")
     
     -- WANMAP settings
     uci:section("network", "interface", "wanmap", {
@@ -298,6 +298,7 @@ end
 
 -- map-e OCN Virtual Connect 接続設定関数
 function configure_mape_ocn(peeraddr, ipv4_prefix, ipv4_prefixlen, ipv6_prefix, ipv6_prefixlen, ealen, psidlen, offset, ipv6_56)
+local ipv6_56 = wan_ipv6
       
     -- DHCP LAN settings
     uci:set("dhcp", "lan", "dhcp")

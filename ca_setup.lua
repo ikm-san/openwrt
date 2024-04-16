f33a795a2386a2ab1963308e09224405aa84c357
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-04-16 09:55:55 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -1,7 +1,7 @@
 module("luci.controller.ca_setup", package.seeall)
 
 function index()
-    -- CA SETUPのトップレベルエントリを作成
+    -- CA SETUPのトップレベルエントリ
     entry({"admin", "ca_setup"}, firstchild(), _("CA接続設定"), 60).dependent = false
     
     -- IPoE設定タブ
@@ -9,9 +9,6 @@ function index()
 
     -- WiFi設定タブ
     entry({"admin", "ca_setup", "wireless"}, cbi("ca_setup/wireless"), _("WiFi接続設定"), 20)
-
-    -- アップデート処理
-    entry({"admin", "ca_setup", "update"}, cbi("ca_setup/update"), _("最新版へ更新処理"), 30)
     
     -- デバッグ用
     -- entry({"admin", "ca_setup", "sandbox"}, cbi("ca_setup/sandbox"), _("テスト用"), 60)

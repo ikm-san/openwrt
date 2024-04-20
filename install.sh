f32bb4ee05e79c2fa2dd819f6e89c66caa696866
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-04-20 11:42:12 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ mkdir -p /usr/lib/lua/luci/controller/
 mkdir -p /usr/lib/lua/luci/model/cbi/ca_setup/
 
 # Download configuration files and scripts
-wget -O /etc/config/ca_setup "https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup"
-wget -O /usr/lib/lua/luci/controller/ca_setup.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua"
-wget -O /usr/lib/lua/calib.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/calib.lua"
-wget -O /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua"
-wget -O /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/wireless.lua"
+curl -o /etc/config/ca_setup "https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup"
+curl -o /usr/lib/lua/luci/controller/ca_setup.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/ca_setup.lua"
+curl -o /usr/lib/lua/calib.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/calib.lua"
+curl -o /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua"
+curl -o /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/wireless.lua"
 
 # Clear Luci cache
 rm -rf /tmp/luci-*

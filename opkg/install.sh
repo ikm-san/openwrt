66ea7659a67aaf4f7d1ad26648a34d1681569159
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-05-05 15:34:01 +0900

    Update install.sh

--- opkg/install.sh
+++ opkg/install.sh
@@ -10,10 +10,3 @@ curl -o /usr/lib/lua/luci/controller/ca_setup.lua "https://raw.githubusercontent
 curl -o /usr/lib/lua/calib.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/calib.lua"
 curl -o /usr/lib/lua/luci/model/cbi/ca_setup/ipoe.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/ipoe.lua"
 curl -o /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua "https://raw.githubusercontent.com/ikm-san/openwrt/main/wireless.lua"
-
-# Clear Luci cache
-rm -rf /tmp/luci-*
-
-# Restart uHTTPd server
-/etc/init.d/uhttpd restart
-

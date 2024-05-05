4182d20a33a0b25b78e865cc6e07286c2f5f35ca
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-05-05 15:33:43 +0900

    Update uninstall.sh

--- opkg/uninstall.sh
+++ opkg/uninstall.sh
@@ -10,9 +10,3 @@ rm -f /usr/lib/lua/luci/model/cbi/ca_setup/wireless.lua
 # Remove created directories (and their contents)
 rm -rf /usr/lib/lua/luci/controller/
 rm -rf /usr/lib/lua/luci/model/cbi/ca_setup/
-
-# Clear Luci cache
-rm -rf /tmp/luci-*
-
-# Optionally restart uHTTPd server if needed
-/etc/init.d/uhttpd restart

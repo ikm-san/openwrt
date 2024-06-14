906fe7db444867126e8d3fd344dc3808b44351bd
ikm-san <satoshi.ikeuchi3104@gmail.com>
2024-06-14 16:08:12 +0900

    Update ipoe.lua

--- ipoe.lua
+++ ipoe.lua
@@ -1,7 +1,6 @@
 
 local sys = require "luci.sys"
 local uci = require "luci.model.uci".cursor()
-local uci = require "luci.model.uci".cursor()
 local fs = require "nixio.fs"
 local json = require("luci.jsonc")
 local ubus = require "ubus"

0784fd7b780b33a4e120361481bd76c726ecb2ba
ikm-san <160682436+ikm-san@users.noreply.github.com>
2024-08-09 13:58:22 +0900

    Update ca_setup.lua

--- ca_setup.lua
+++ ca_setup.lua
@@ -8,7 +8,9 @@ function index()
     entry({"admin", "ca_setup", "ipoe"}, cbi("ca_setup/ipoe"), _("Internet接続設定"), 10)
 
     -- WiFi設定タブ
-    entry({"admin", "ca_setup", "wireless"}, cbi("ca_setup/wireless"), _("WiFi接続設定"), 20)
+    if nixio.fs.access("/etc/config/wireless") then
+        entry({"admin", "ca_setup", "wireless"}, cbi("ca_setup/wireless"), _("WiFi接続設定"), 20)
+    end
     
     -- デバッグ用
     -- entry({"admin", "ca_setup", "sandbox"}, cbi("ca_setup/sandbox"), _("テスト用"), 60)
